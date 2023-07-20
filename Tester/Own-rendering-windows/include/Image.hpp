da3de728a4533b43c5027cd3ff910039351b3c5f
Hugo PREVOST <hugo.prevost@epitech.eu>
2023-07-20 20:53:35 +0200

    feat(Tester): implementating Image::getPixel

--- Tester/Own-rendering-windows/include/Image.hpp
+++ Tester/Own-rendering-windows/include/Image.hpp
@@ -5,6 +5,7 @@
 #include <string>
 
 #include "Engine/Point2.hpp"
+#include "Engine/Color.hpp"
 
 #define EXT(_path) _path.substr(_path.rfind(".") + 1)
 
@@ -14,11 +15,12 @@ namespace tester
     {
         public:
             Image() = default;
-            ~Image() = default;
+            ~Image();
 
             void load(const std::string &_path);
 
             [[nodiscard]] Point2<uint32_t> getSize() const;
+            [[nodiscard]] Color getPixel(const Point2<uint32_t> &_pos) const;
 
         private:
             friend class RenderTarget;
@@ -26,5 +28,7 @@ namespace tester
             void facBmp(const std::string &_path);
 
             HBITMAP m_bmp;
+            std::vector<uint8_t> m_pxl;
+            Point2<uint32_t> m_size = { 0, 0 };
     };
 }
